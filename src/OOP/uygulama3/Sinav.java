78d151ad2c555cd313c022fcb3d11465f3d820ce
Adanedhel <furkan.peon@gmail.com>
2023-04-04 14:15:04 +0300

    04 / 04 / 2023

--- src/OOP/uygulama3/Sinav.java
+++ src/OOP/uygulama3/Sinav.java
@@ -3,6 +3,9 @@ package OOP.uygulama3;
 import java.util.Scanner;
 
 public class Sinav {
+	public String ders;
+	public String konu;
+	
 	public String sinavAdi;	
 	public int soruSayisi;
 	public long suresi;
@@ -18,6 +21,7 @@ public class Sinav {
 		this.soruSayisi=input.nextInt();input.nextLine();//enter hava kalmasın diye yapılan bir olay
 		System.out.println("Lütfen sinav süresini giriniz: ");
 		this.suresi=input.nextLong();input.nextLine();
+		this.cevaplar = new String[sorular.length];
 		sorular = new Soru[soruSayisi];
 		for (int i = 0; i < soruSayisi; i++) {
 			Soru soru=new Soru();
@@ -27,6 +31,37 @@ public class Sinav {
 		}
 		
 		
+		
+	}
+	public void cevaplariGir() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Ders: " + this.ders );
+		System.out.println("Konu: " + this.konu );
+		System.out.println("Sure: " + this.suresi );
+		for (int i = 0; i < sorular.length; i++) {
+			System.out.println(sorular[i].icerik);
+			for (int j = 0; j < sorular[i].cevapIcerikleri.length; j++) {
+				System.out.println(cevapSecenekleri[i] +"-)"+ sorular[i].cevapIcerikleri[j]);
+			}
+			System.out.println("Cevabinizi giriniz: A,B,C,D");
+			cevaplar[i] = input.nextLine().toUpperCase();
+		}
+	}
+	
+	public void cevaplariKontrolEt() {
+		int dogruSayisi=0;
+		int puan=0;
+		
+		for (int i = 0; i < sorular.length; i++) {
+			if (cevaplar[i].equals(sorular[i].dogruCevap)) {
+				dogruSayisi++;
+				puan +=sorular[i].puan;
+			}
+		}
+		
+		System.out.println("dogru cevap sayisi " + dogruSayisi);
+		System.out.println("yanlış cevap sayisi " + (soruSayisi-dogruSayisi));
+		System.out.println("puan " + puan);
 	}
 		
 	
